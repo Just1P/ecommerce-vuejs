67044aa7effd68df17b3be24e0228a00d80481f6
Pitra <justin.pitra@gmail.com>
2024-05-01 20:46:14 +0200

    setup script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 const { createApp, ref } = Vue;
-import {cart} from "./components/cart.js";
-import {navigation} from "./components/cart.js";
-import {product} from "./components/cart.js";
+import { cart } from "./components/cart.js";
+import { navigation } from "./components/cart.js";
+import { product } from "./components/cart.js";
 const vm = createApp({
-    components: {
-      "cards": product,
-      "nav-bar": navigation,
-      "shop": cart,
-    },
-    data() {
-      return {
-        productList: [],
-      };
-    },
-    methods: {
-      
-      },
-    },
-    mounted() {
-      fetch("products.json")
-        .then((response) => response.json())
-        .then((data) => {
-          this.productList = data;
-        });
-    },
-  }).mount("#app");
+  components: {
+    cards: product,
+    "nav-bar": navigation,
+    shop: cart,
+  },
+  data() {
+    return {
+      productList: [],
+    };
+  },
+  methods: {},
+  mounted() {
+    fetch("products.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.productList = data;
+      });
+  },
+}).mount("#app");
