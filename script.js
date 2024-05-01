aa68863b32431e984a7b451fcd854fd49d22f0eb
Pitra <justin.pitra@gmail.com>
2024-05-01 20:41:27 +0200

    setup index

--- script.js
+++ script.js
@@ -4,36 +4,24 @@ import {navigation} from "./components/cart.js";
 import {product} from "./components/cart.js";
 const vm = createApp({
     components: {
-      "card": cardItem,
-      "form-component": FormComponent,
+      "cards": product,
+      "nav-bar": navigation,
+      "shop": cart,
     },
     data() {
       return {
-        postList: [],
+        productList: [],
       };
     },
     methods: {
-      handleSubmit(title, content) {
-        const newPost = {
-          id: 10,
-          title: title,
-          content: content,
-          image_url:
-            "https://cdn.pixabay.com/photo/2023/04/06/07/45/rose-7903170_1280.jpg",
-          like: 0,
-        };
-        this.postList.push(newPost);
-      },
-      increase(id) {
-        console.log("Increase likes for post with id :", id);
-        this.postList.find((post) => post.id === id).like += 1;
+      
       },
     },
     mounted() {
-      fetch("data.json")
+      fetch("products.json")
         .then((response) => response.json())
         .then((data) => {
-          this.postList = data;
+          this.productList = data;
         });
     },
   }).mount("#app");
