12565613843dde5985d45fc1860d824b720409cc
Pitra <justin.pitra@gmail.com>
2024-05-01 21:40:46 +0200

    le vrai bon js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const { createApp, ref } = Vue;
-<<<<<<< HEAD
 import {cart} from "./components/cart.js";
 import {navigation} from "./components/navigation.js";
 import {product} from "./components/products.js";
@@ -24,28 +23,3 @@ const vm = createApp({
         });
     },
   }).mount("#app");
-=======
-import { cart } from "./components/cart.js";
-import { navigation } from "./components/cart.js";
-import { product } from "./components/cart.js";
-const vm = createApp({
-  components: {
-    cards: product,
-    "nav-bar": navigation,
-    shop: cart,
-  },
-  data() {
-    return {
-      productList: [],
-    };
-  },
-  methods: {},
-  mounted() {
-    fetch("products.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.productList = data;
-      });
-  },
-}).mount("#app");
-
