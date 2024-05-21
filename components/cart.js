7869018fc799159644203b1abf2d5c6b97dc7a4d
Pitra <justin.pitra@gmail.com>
2024-05-21 23:51:31 +0200

    tout fonctionne !!!

--- components/cart.js
+++ components/cart.js
@@ -1,3 +1,23 @@
 export const cart = {
-   
-}
+  props: {
+    id: Number,
+    img_src: String,
+    price: Number,
+    name: String,
+    number: Number,
+  },
+  template: `
+    <div class="cart-container" :key="id">
+      <div class="img-container">
+        <img :src="img_src" alt="" class="cart-img" >
+      </div>
+      <div class="content">
+        <div class="cart-title"><p class="product-name">{{ name }}</p></div>
+        <div class="cart-price">{{ price }} € x {{ number }}</div>
+      </div>
+      <button class="trash" @click="$emit('removeOnCart', id)">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+    </div>
+  `,
+};
--- components/navigation.js
+++
@@ -1,6 +0,0 @@
-export const navigation = {
-  template: `
-        <i class="fa-solid fa-heart nav-icon"></i>
-        <i class="fa-solid fa-cart-shopping nav-icon"></i>
-    `,
-};
